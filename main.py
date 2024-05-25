63863d8f7e5b19234ab0ea01cd1dab952cb341fd
David Sedlák <isedlak@fit.vut.cz>
2024-05-25 20:23:52 +0200

    sym. regression add support for higher dimensionality

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy
 
 from functools import partial
 
+import pmlb
 from deap import algorithms
 from deap import base
 from deap import creator
@@ -34,10 +35,23 @@ def evalSymbReg(individual, points):
     return math.fsum(sqerrors) / len(points),
 
 
+def eval_symb_reg_pmlb(individual, inputs, targets):
+    func = toolbox.compile(expr=individual)
+    #data = fetch_data('adult')
+    #inputs = data.drop('target', axis=1)
+    #targets = data['target']
+
+    outputs = inputs.apply(lambda x: func(*x), axis=1)# engine="numba")
+    return ((outputs - targets) ** 2).sum() / len(targets),
+
+
 def main():
     random.seed(318)
 
-    pset = gp.PrimitiveSet("MAIN", 1)
+    dataset_name = 'adult'
+    dataset = pmlb.fetch_data(dataset_name)
+
+    pset = gp.PrimitiveSet("MAIN", len(dataset.drop('target', axis=1).columns))
     pset.addPrimitive(operator.add, 2)
     pset.addPrimitive(operator.sub, 2)
     pset.addPrimitive(operator.mul, 2)
@@ -46,7 +60,7 @@ def main():
     pset.addPrimitive(math.cos, 1)
     pset.addPrimitive(math.sin, 1)
     pset.addEphemeralConstant("rand101", partial(random.randint, -1, 1))
-    pset.renameArguments(ARG0='x')
+    #pset.renameArguments(ARG0='x')
 
     creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
     creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
@@ -56,7 +70,9 @@ def main():
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("compile", gp.compile, pset=pset)
 
-    toolbox.register("evaluate", evalSymbReg, points=[x / 10. for x in range(-10, 10)])
+    #toolbox.register("evaluate", evalSymbReg, points=[x / 10. for x in range(-10, 10)])
+    toolbox.register("evaluate", eval_symb_reg_pmlb, inputs=dataset.drop('target', axis=1),
+                     targets=dataset['target'])
     toolbox.register("select", tools.selTournament, tournsize=3)
     toolbox.register("mate", gp.cxOnePoint)
     toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
@@ -65,7 +81,7 @@ def main():
     toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
     toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
 
-    pop = toolbox.population(n=300)
+    pop = toolbox.population(n=10)
     hof = tools.HallOfFame(1)
 
     stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
