2d370f151a102607269ca03900ad23b1d0cd3fb5
David Sedlák <isedlak@fit.vut.cz>
2024-06-02 15:39:41 +0200

    fix mask mismatch

--- train_model.py
+++ train_model.py
@@ -36,7 +36,7 @@ def train_loop(model, opt, loss_fn, dataloader):
 
         # Get mask to mask out the next words
         sequence_length = y_input.size(1)
-        tgt_mask = model.get_tgt_mask(sequence_length).to(device)
+        tgt_mask = model.get_tgt_mask(sequence_length)
 
         src_key_padding_mask = (x == PAD_TOKEN).to(torch.bool)
         tgt_key_padding_mask = (y_input == PAD_TOKEN).to(torch.bool)
