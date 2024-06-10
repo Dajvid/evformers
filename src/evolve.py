7c55c06f04ea00834a3d79b2ae4213ef09e429be
David Sedlák <isedlak@fit.vut.cz>
2024-06-10 22:24:45 +0200

    create basic symreg experiment toolkit

---
+++ src/evolve.py
@@ -0,0 +1,101 @@
+import argparse
+import operator
+import os.path
+
+import numpy as np
+import pandas as pd
+import pmlb
+
+from deap import creator, base, gp, tools
+from deap.algorithms import eaSimple
+
+from gp.Fitness import eval_symb_reg_pmlb
+from gp.Pset import create_basic_symreg_pset
+from gp.sym_reg_tree import SymRegTree
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pop-size", type=int, default=50)
+    parser.add_argument("--generations", type=int, default=100)
+    parser.add_argument("--max-depth", type=int, default=8)
+    parser.add_argument("--min-depth", type=int, default=0)
+    parser.add_argument("--p-cross", type=float, default=0.7)
+    parser.add_argument("--p-mut", type=float, default=0.3)
+    parser.add_argument("--crossover_operator", type=str)
+    parser.add_argument("--mutation_operator", type=str)
+    parser.add_argument("--tournament-size", type=int, default=3)
+    parser.add_argument("--dataset", type=str, default="505_tecator")
+    parser.add_argument("--output-dir", type=str, default="../runs/evolution")
+    parser.add_argument("--verbose", type=bool, action=argparse.BooleanOptionalAction, default=False)
+    parser.add_argument("--run-id", type=int, default=0)
+
+    return parser.parse_args(argv)
+
+
+def main(argv=None):
+    args = parse_args(argv)
+
+    dataset = pmlb.fetch_data(args.dataset)
+    pset = create_basic_symreg_pset(dataset)
+
+    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    creator.create("Individual", SymRegTree, fitness=creator.FitnessMin, pset=pset)
+
+    toolbox = base.Toolbox()
+    toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=args.min_depth, max_=args.max_depth)
+    toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("compile", gp.compile, pset=pset)
+    toolbox.pset = pset
+    toolbox.register("evaluate", eval_symb_reg_pmlb, inputs=dataset.drop('target', axis=1),
+                     targets=dataset['target'])
+    toolbox.register("select", tools.selTournament, tournsize=args.tournament_size)
+    toolbox.register("mate", gp.cxOnePoint)
+    toolbox.register("expr_mut", gp.genFull, min_=args.min_depth, max_=args.max_depth)
+    toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+    toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"),
+                                            max_value=args.max_depth))
+    toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"),
+                                              max_value=args.max_depth))
+
+    pop = toolbox.population(n=args.pop_size)
+    hof = tools.HallOfFame(1)
+    stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_fit.register("fit-avg", np.mean)
+    stats_fit.register("fit-std", np.std)
+    stats_fit.register("fit-min", np.min)
+    stats_fit.register("fit-max", np.max)
+
+    stats_height = tools.Statistics(lambda ind: ind.height)
+    stats_height.register("height-avg", np.mean)
+    stats_height.register("height-std", np.std)
+    stats_height.register("height-min", np.min)
+    stats_height.register("height-max", np.max)
+    mstats = tools.MultiStatistics(fitness=stats_fit, height=stats_height)
+    #mstats = tools.Statistics(lambda ind: ind.fitness.values)
+
+    pop, log = eaSimple(pop, toolbox, args.p_cross, args.p_mut, args.generations, stats=mstats, halloffame=hof,
+                        verbose=args.verbose)
+
+    statistics = pd.DataFrame([{**st1, **st2}for st1, st2 in zip(log.chapters["fitness"], log.chapters["height"])])
+    statistics["run_id"] = args.run_id
+    statistics["dataset"] = args.dataset
+    statistics["pop_size"] = args.pop_size
+    statistics["generations"] = args.generations
+    statistics["max_depth"] = args.max_depth
+    statistics["min_depth"] = args.min_depth
+    statistics["p_cross"] = args.p_cross
+    statistics["p_mut"] = args.p_mut
+    statistics["tournament_size"] = args.tournament_size
+    statistics["crossover_operator"] = args.crossover_operator
+    statistics["mutation_operator"] = args.mutation_operator
+    statistics["output_dir"] = args.output_dir
+
+    os.makedirs(args.output_dir, exist_ok=True)
+    statistics.to_pickle(os.path.join(args.output_dir, f"{args.dataset}_run_{args.run_id}.pkl"))
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/generate_experimen_jsons.py
@@ -0,0 +1,50 @@
+import json
+
+runs_per_experiment = 100
+max_fitness_evaluations = 1000000
+
+
+def generate_variant_over_datasets(variant_name, variants):
+    datasets = ["505_tecator", "588_fri_c4_1000_100", "503_wind", "228_elusage"]
+    experiments = []
+
+    params = {
+        "--pop-size": 50,
+        "--generations": 100,
+        "--max-depth": 8,
+        "--min-depth": 0,
+        "--p-cross": 0.7,
+        "--p-mut": 0.3,
+        "--crossover_operator": "cxOnePoint",
+        "--mutation_operator": "mutUniform",
+        "--tournament-size": 3,
+        "--verbose": False
+    }
+
+    for dataset in datasets:
+        for variant in variants:
+            params["--dataset"] = dataset
+            if variant_name == "--pop-size":
+                params["--generations"] = max_fitness_evaluations // variant
+            elif variant_name == "--generations":
+                params["--pop-size"] = max_fitness_evaluations // variant
+
+            params[variant_name] = variant
+            params["output-dir"] = f"../runs/evolution/{dataset}/{variant_name}-{variant}/"
+            experiments.append({
+                "command": params,
+                "remaining-runs": runs_per_experiment,
+                "requires-gpu": False,
+                "total-runs": runs_per_experiment
+            })
+    return experiments
+
+
+experiments = []
+experiments.extend(generate_variant_over_datasets("--popsize", [1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]))
+experiments.extend(generate_variant_over_datasets("--p-cross", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
+experiments.extend(generate_variant_over_datasets("--p-mut", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
+experiments.extend(generate_variant_over_datasets("--tournament-size", [1, 2, 3, 5, 7, 10, 20, 50]))
+
+with open("../new_expr.json", "w") as experiments_f:
+    experiments_f.write(json.dumps(experiments, indent=4, sort_keys=True))
