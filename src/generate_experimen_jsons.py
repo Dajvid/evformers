6882a53db879e6c901c688d1e39219426067b34b
David Sedlák <isedlak@fit.vut.cz>
2024-06-10 22:56:45 +0200

    fix errors in evolve_experiments generator

--- src/generate_experimen_jsons.py
+++ src/generate_experimen_jsons.py
@@ -9,28 +9,27 @@ def generate_variant_over_datasets(variant_name, variants):
     experiments = []
 
     params = {
-        "--pop-size": 50,
-        "--generations": 100,
-        "--max-depth": 8,
-        "--min-depth": 0,
-        "--p-cross": 0.7,
-        "--p-mut": 0.3,
-        "--crossover_operator": "cxOnePoint",
-        "--mutation_operator": "mutUniform",
-        "--tournament-size": 3,
-        "--verbose": False
+        "--pop-size": str(50),
+        "--generations": str(100),
+        "--max-depth": str(8),
+        "--min-depth": str(0),
+        "--p-cross": str(0.7),
+        "--p-mut": str(0.3),
+        "--crossover-operator": "cxOnePoint",
+        "--mutation-operator": "mutUniform",
+        "--tournament-size": str(3)
     }
 
     for dataset in datasets:
         for variant in variants:
             params["--dataset"] = dataset
             if variant_name == "--pop-size":
-                params["--generations"] = max_fitness_evaluations // variant
+                params["--generations"] = str(max_fitness_evaluations // variant)
             elif variant_name == "--generations":
-                params["--pop-size"] = max_fitness_evaluations // variant
+                params["--pop-size"] = str(max_fitness_evaluations // variant)
 
-            params[variant_name] = variant
-            params["output-dir"] = f"../runs/evolution/{dataset}/{variant_name}-{variant}/"
+            params[variant_name] = str(variant)
+            params["--output-dir"] = f"../runs/evolution/{dataset}/{variant_name}-{variant}/"
             experiments.append({
                 "command": [item for pair in params.items() for item in pair],
                 "remaining-runs": runs_per_experiment,
@@ -41,7 +40,7 @@ def generate_variant_over_datasets(variant_name, variants):
 
 
 experiments = []
-experiments.extend(generate_variant_over_datasets("--popsize", [1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]))
+experiments.extend(generate_variant_over_datasets("--pop-size", [1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]))
 experiments.extend(generate_variant_over_datasets("--p-cross", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
 experiments.extend(generate_variant_over_datasets("--p-mut", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
 experiments.extend(generate_variant_over_datasets("--tournament-size", [1, 2, 3, 5, 7, 10, 20, 50]))
