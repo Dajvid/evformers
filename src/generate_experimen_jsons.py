34f3f385809f9d08298b576aac2d6c7f256b0244
David Sedlák <isedlak@fit.vut.cz>
2024-06-11 11:00:34 +0200

    update evo experiments

--- src/generate_experimen_jsons.py
+++ src/generate_experimen_jsons.py
@@ -1,11 +1,12 @@
 import json
 
 runs_per_experiment = 100
-max_fitness_evaluations = 1000000
+max_fitness_evaluations = 30000
 
 
 def generate_variant_over_datasets(variant_name, variants):
-    datasets = ["505_tecator", "588_fri_c4_1000_100", "503_wind", "228_elusage"]
+    #datasets = ["505_tecator", "588_fri_c4_1000_100", "503_wind", "228_elusage"]
+    datasets = ["505_tecator"]
     experiments = []
 
     params = {
@@ -29,7 +30,7 @@ def generate_variant_over_datasets(variant_name, variants):
                 params["--pop-size"] = str(max_fitness_evaluations // variant)
 
             params[variant_name] = str(variant)
-            params["--output-dir"] = f"../runs/evolution/{dataset}/{variant_name}-{variant}/"
+            params["--output-dir"] = f"../runs/evolution/{variant_name}/{dataset}/{variant_name}-{variant}/"
             experiments.append({
                 "command": [item for pair in params.items() for item in pair],
                 "remaining-runs": runs_per_experiment,
@@ -40,10 +41,10 @@ def generate_variant_over_datasets(variant_name, variants):
 
 
 experiments = []
-experiments.extend(generate_variant_over_datasets("--pop-size", [1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]))
+#experiments.extend(generate_variant_over_datasets("--pop-size", [1, 5, 10, 20, 50, 100, 200, 500, 1000, 2000]))
 experiments.extend(generate_variant_over_datasets("--p-cross", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
-experiments.extend(generate_variant_over_datasets("--p-mut", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
-experiments.extend(generate_variant_over_datasets("--tournament-size", [1, 2, 3, 5, 7, 10, 20, 50]))
+# experiments.extend(generate_variant_over_datasets("--p-mut", [0.01, 0.1, 0.2, 0.5, 0.7, 0.8, 0.95, 1]))
+# experiments.extend(generate_variant_over_datasets("--tournament-size", [1, 2, 3, 5, 7, 10, 20, 50]))
 
 with open("../new_expr.json", "w") as experiments_f:
     experiments_f.write(json.dumps(experiments, indent=4, sort_keys=True))
