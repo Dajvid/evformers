9af4598799e9af770e7e360ada63af2c56f2b12d
David Sedlák <isedlak@fit.vut.cz>
2024-06-17 10:27:56 +0200

    new plots

--- src/draw_plots.py
+++ src/draw_plots.py
@@ -7,13 +7,13 @@ from pathlib import Path
 
 import torch
 
-from gp.Pset import create_basic_symreg_pset
+from gp.Pset import create_basic_symreg_pset, create_basic_logic_pset
 from gp.sym_reg_tree import SymRegTree, get_mapping
 from gpformer.model import Transformer
+from gp.Generate_data import even_parity_truth_table
 
 
-def plot_evolution_results(results: pd.DataFrame, output_dir: str):
-    pass
+figsize = (7.3, 4.4)
 
 
 def add_property(folder: str, property: str, value: float):
@@ -56,9 +56,42 @@ def plot_data(df: pd.DataFrame, feature: str, x_label: str, path: str = None):
         plt.savefig(path)
 
 
+def plot_convergence(data, xlabel, ylabel, out_path, fit_evaluations=10000, logscale=False, visualize=False):
+    """
+    Plot convergence of data and save the plot to a file.
+
+    :param data (dict): A dictionary containing the data to be plotted. The keys represent labels, and the values are
+            numpy arrays of data points.
+    :param xlabel: The label for the x-axis.
+    :parame ylabel: The label for the y-axis.
+    :param out_path: The file path to save the plot.
+    :param logscale: If True, the x-axis is plotted on a logarithmic scale. Defaults to False.
+    :param visualize: If True, display the plot interactively. Defaults to False.
+    """
+    plt.figure(figsize=figsize)
+    plt.plot()
+    for key, values in data.items():
+        #grouped = values.groupby(["gen", "run_id"]).agg({"fit-min": ["min", "avg", "max"]})
+        values = values.groupby(["gen"]).agg({"fit-min": ["min", "mean", "max"]}).reset_index()
+        x = np.linspace(1, fit_evaluations, num=len(values), dtype=np.int64)
+        if logscale:
+            plt.xscale('log')
+        plt.plot(x, values["fit-min", "mean"], label=key, scalex=False)
+        plt.fill_between(x, values["fit-min", "min"], values["fit-min", "max"], alpha=0.3)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.savefig(out_path, bbox_inches='tight')
+    if visualize:
+        plt.show()
+    plt.close()
+
+
 def similarity_heatmap(model_weights="../model-tecator-0-6-SOT.pth", dataset="505_tecator", SOT=True, masked=False,
                        measure="cos_dist", ignore_pad=False, aggregate=None):
-    pset = create_basic_symreg_pset(pmlb.fetch_data(dataset))
+    #pset = create_basic_symreg_pset(pmlb.fetch_data(dataset))
+    pset = create_basic_logic_pset(even_parity_truth_table(3))
     extra_elements = ["PAD", "UNKNOWN"]
     if SOT:
         extra_elements.append("SOT")
@@ -67,30 +100,44 @@ def similarity_heatmap(model_weights="../model-tecator-0-6-SOT.pth", dataset="50
         max_token_id = max(list(mapping.values()))
         mapping["MASK"] = max_token_id + 1
 
+    # trees = {
+    #     "T1": SymRegTree.from_string("add(ARG0, 1.0)", pset=pset),
+    #     "T1R": SymRegTree.from_string("add(1.0, ARG0)", pset=pset),
+    #     "T1EQ": SymRegTree.from_string("add(ARG0, add(0.5, 0.5))", pset=pset),
+    #
+    #     "T2": SymRegTree.from_string("div(ARG0, 0.5)", pset=pset),
+    #     "T2R": SymRegTree.from_string("div(0.5, ARG0)", pset=pset),
+    #     "T2EQ": SymRegTree.from_string("mul(ARG0, add(1.0, 1.0))", pset=pset),
+    #
+    #     "T3": SymRegTree.from_string("mul(ARG0, 0.5)", pset=pset),
+    #     "T3R": SymRegTree.from_string("mul(0.5, ARG0)", pset=pset),
+    #     "T3EQ": SymRegTree.from_string("div(ARG0, add(1.0, 1.0))", pset=pset),
+    #
+    #     "T4": SymRegTree.from_string("add(div(ARG0, 0.5), 1.0)", pset=pset),
+    #     "T4R": SymRegTree.from_string("add(1.0, div(ARG0, 0.5))", pset=pset),
+    #
+    #     "T5": SymRegTree.from_string("div(ARG0, div(0.20000000000000018, ARG0))", pset=pset),
+    #     "T5R": SymRegTree.from_string("div(div(0.20000000000000018, ARG0), ARG0)", pset=pset),
+    #
+    #     "T6": SymRegTree.from_string("add(mul(ARG0, div(ARG0, 0.5)), 1.0)", pset=pset),
+    #     "T6R": SymRegTree.from_string("add(1.0, mul(ARG0, div(ARG0, 0.5)))", pset=pset),
+    #
+    #     "T7": SymRegTree.from_string("sub(mul(ARG0, 0.30000000000000004), sub(ARG0, 0.8))", pset=pset),
+    #     "T7R": SymRegTree.from_string("sub(sub(ARG0, 0.8), mul(ARG0, 0.30000000000000004))", pset=pset),
+    # }
+
     trees = {
-        "T1": SymRegTree.from_string("add(ARG0, 1.0)", pset=pset),
-        "T1R": SymRegTree.from_string("add(1.0, ARG0)", pset=pset),
-        "T1EQ": SymRegTree.from_string("add(ARG0, add(0.5, 0.5))", pset=pset),
+        "T1": SymRegTree.from_string("And(ARG0, True)", pset=pset),
+        "T1R": SymRegTree.from_string("And(True, ARG0)", pset=pset),
+        "T1EQ": SymRegTree.from_string("And(Not(False), ARG0)", pset=pset),
 
-        "T2": SymRegTree.from_string("div(ARG0, 0.5)", pset=pset),
-        "T2R": SymRegTree.from_string("div(0.5, ARG0)", pset=pset),
-        "T2EQ": SymRegTree.from_string("mul(ARG0, add(1.0, 1.0))", pset=pset),
+        "T2": SymRegTree.from_string("Or(ARG0, ARG2)", pset=pset),
+        "T2R": SymRegTree.from_string("Or(ARG2, ARG0)", pset=pset),
+        "T2EQ": SymRegTree.from_string("Or(Not(Not(ARG2)), ARG0)", pset=pset),
 
-        "T3": SymRegTree.from_string("mul(ARG0, 0.5)", pset=pset),
-        "T3R": SymRegTree.from_string("mul(0.5, ARG0)", pset=pset),
-        "T3EQ": SymRegTree.from_string("div(ARG0, add(1.0, 1.0))", pset=pset),
-
-        "T4": SymRegTree.from_string("add(div(ARG0, 0.5), 1.0)", pset=pset),
-        "T4R": SymRegTree.from_string("add(1.0, div(ARG0, 0.5))", pset=pset),
-
-        "T5": SymRegTree.from_string("div(ARG0, div(0.20000000000000018, ARG0))", pset=pset),
-        "T5R": SymRegTree.from_string("div(div(0.20000000000000018, ARG0), ARG0)", pset=pset),
-
-        "T6": SymRegTree.from_string("add(mul(ARG0, div(ARG0, 0.5)), 1.0)", pset=pset),
-        "T6R": SymRegTree.from_string("add(1.0, mul(ARG0, div(ARG0, 0.5)))", pset=pset),
-
-        "T7": SymRegTree.from_string("sub(mul(ARG0, 0.30000000000000004), sub(ARG0, 0.8))", pset=pset),
-        "T7R": SymRegTree.from_string("sub(sub(ARG0, 0.8), mul(ARG0, 0.30000000000000004))", pset=pset),
+        "T3": SymRegTree.from_string("And(Or(ARG0, ARG1), True)", pset=pset),
+        "T3R": SymRegTree.from_string("And(True, Or(ARG0, ARG1))", pset=pset),
+        "T3EQ": SymRegTree.from_string("And(Or(ARG0, ARG1), Or(ARG2, Not(False)))", pset=pset)
     }
     model = Transformer(mapping, 2 * 6, 2, 1, 1,
                         6, 2, ignore_pad=ignore_pad).to("cpu")
@@ -147,9 +194,17 @@ def benchmark_de_mut_cxOnePoint():
     # print("Mutation succes rate: ", df["mut_success"].sum() / df["mut_called"].sum())
     # plot_data(df, "mut_ratio_param", "CR", "../plots/de_mut_cxOnePoint_mut_ratio_param.pdf")
 
-    df = load_data("../runs/evolution/de_mut_cxOnePoint/--p-mut")
+    # df = load_data("../runs/evolution/de_mut_cxOnePoint/--p-mut")
+    # print("Mutation succes rate: ", df["mut_success"].sum() / df["mut_called"].sum())
+    # plot_data(df, "p_mut", "Mutation probability", "../plots/de_mut_cxOnePoint_p_mut.pdf")
+
+    df = load_data("../runs/evolution/de_mut_cxOnePoint/--p-cross")
     print("Mutation succes rate: ", df["mut_success"].sum() / df["mut_called"].sum())
-    plot_data(df, "p_mut", "Mutation probability", "../plots/de_mut_cxOnePoint_p_mut.pdf")
+    plot_data(df, "p_cross", "Crossover probability", None)
+
+    df = load_data("../runs/evolution/de_mut_cxOnePoint/--tournament-size")
+    print("Mutation succes rate: ", df["mut_success"].sum() / df["mut_called"].sum())
+    plot_data(df, "tournament_size", "Tournament size", None)
 
 
 def benchmark_mutUniform_cxOnePoint():
@@ -194,8 +249,12 @@ def model_plots():
 def main():
     # similarity_heatmaps()
     #model_plots()
-    #benchmark_de_mut_cxOnePoint()
-    benchmark_mutUniform_cxOnePoint()
+    # similarity_heatmap("../training-runs/2024-06-15_16-21-40_dhcpg197.fit.vutbr.cz/model.pth", None,
+    #                    ignore_pad=False, SOT=True, masked=True)
+    # similarity_heatmap("../training-runs/2024-06-16_16-32-26_dhcpg197.fit.vutbr.cz/model.pth", None,
+    #                    ignore_pad=False, SOT=True, masked=False)
+    # benchmark_de_mut_cxOnePoint()
+    #benchmark_mutUniform_cxOnePoint()
     # similarity_heatmap("../training-runs/2024-06-13_15-32-01_dhcpg197.fit.vutbr.cz/model.pth",
     #                    "505_tecator", ignore_pad=False, SOT=True, masked=True, aggregate="sum")
     # similarity_heatmap("../training-runs/2024-06-13_17-03-03_dhcpf244.fit.vutbr.cz/model.pth",
@@ -216,10 +275,16 @@ def main():
     # plot_data(df, "mut_param")
     #
     #
-    # df = load_data("../runs/evolution/mut_rev_cosine_dist_cxOnePoint/--mut-param")
-    # plot_data(df, "mut_param")
+    df = load_data("../runs/evolution/mut_rev_cosine_dist_cxOnePoint/--mut-param")
+    plot_data(df, "mut_param", "Distance")
     # df = load_data("../runs/evolution/--p-cross")
     # plot_data(df, "p_cross")
+
+    # mutUniform_cxOnePoint = load_data("../runs/evolution/mutUniform_cxOnePoint/--tournament-size/505_tecator/--tournament-size-7")
+    # de_mut_cxOnePoint = load_data("../runs/evolution/de_mut_cxOnePoint/--tournament-size/505_tecator/--tournament-size-7")
+    # plot_convergence({"mutUniform_cxOnePoint": mutUniform_cxOnePoint,
+    #                        "de_mut_cxOnePoint_tournament_size_7": de_mut_cxOnePoint},
+    #                  "Generation", "Fitness", "../plots/de_mut_cxOnePoint_convergence.pdf", visualize=True)
 
 
 if __name__ == "__main__":
